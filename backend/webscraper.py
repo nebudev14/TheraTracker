346f7495b264a51c3ff6743e6488617cb2cdc0cc
warren yun <xxwarrenhdxx@gmail.com>
2021-05-21 17:52:38 -0400

    finished first version of web scraping function

--- backend/webscraper.py
+++ backend/webscraper.py
@@ -3,13 +3,39 @@ from urllib.request import Request, urlopen
 
 link = 'https://www.psychologytoday.com/us/therapists?search=11366'
 
-req = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
-page = urlopen(req)
-page_soup = BeautifulSoup(page.read(), "html.parser")
-page.close()
+def scrape_locations(url):
+    # scraping all the therapists from website
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    page = urlopen(req)
+    page_soup = BeautifulSoup(page.read(), "html.parser")
+    page.close()
 
-divs = page_soup.findAll('div', attrs={'class':'result-row normal-result row'})
-for i in divs:
-    print(i['data-prof-name'])
-    
-# data-result-url
+    # scraping all links to therapist's personal page
+    therapist_links = []
+    therapist_locations = []
+    therapist_home_page = page_soup.findAll('div', attrs={'class':'result-row normal-result row'})
+    for therapist in therapist_home_page:
+        therapist_links.append(therapist['data-result-url'])
+    num = 0
+    # grabbing the office location from each therapist using personal page
+    for urls in therapist_links:
+        address = ""
+        if num > 3:
+            break
+        req = Request(urls, headers={'User-Agent': 'Mozilla/5.0'})
+        page = urlopen(req)
+        page_soup = BeautifulSoup(page.read(), "html.parser")
+        page.close()
+        try:
+            street = page_soup.find('span', attrs={'itemprop':'streetAddress'})
+        except:
+            street = page_soup.find('div', attrs={'class':'location-address-phone'})
+        if street is None: 
+            continue
+        locality = page_soup.find('span', attrs={'itemprop':'addressLocality'})
+        region = page_soup.find('span', attrs={'itemprop':'addressRegion'})
+        postal_code = page_soup.find('span', attrs={'itemprop':'postalcode'})
+        num +=1    
+        address += street.text.strip() + " " + locality.text.strip() + ", " + region.text.strip() + " " + postal_code.text.strip()
+        therapist_locations.append(address)
+    return therapist_locations
