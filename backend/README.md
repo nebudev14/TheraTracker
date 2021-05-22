11ba638b032b0b2ea31e863418ad3eff58fc6e79
warren yun <xxwarrenhdxx@gmail.com>
2021-05-21 22:13:26 -0400

    modified server file a bit, added new readme file

---
+++ backend/README.md
@@ -0,0 +1,2 @@
+# TheraTracker Backend
+Backend for TheraTracker. Server built with flask. Webscraping built with BeautifulSoup. 
--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,11 @@
-from TheraTracker.backend.webscraper import scrape_therapists
 from flask import Flask, jsonify, request 
 from webscraper import scrape_therapists
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
-def index():
-    return str(scrape_therapists('https://www.psychologytoday.com/us/therapists?search=11366'))
+@app.route("/locations/<address>", methods=['GET'])
+def get_location(address):
+    return jsonify({'result': address})
 
 if __name__ == '__main__':
     app.run(debug=True)
