4a27fe00495ea70e2f26463a774f8c459693579a
warren yun <xxwarrenhdxx@gmail.com>
2021-05-22 15:45:24 -0400

    set up location services, set map to center on client location automatically

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 function App() {
   return (
-    <div style={{width: '100vw', height: '100vh'}}>
+    <div style={{width: '80vw', height: '90vh'}}>
       <WrappedMap googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
       loadingElement={<div style={{ height: "100%" }} />}
       containerElement={<div style={{ height: "100%" }} />}
--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,10 +1,37 @@
 import React from 'react';
-import { GoogleMap } from 'react-google-maps';
+import { GoogleMap, LoadScript } from 'react-google-maps';
+import axios from 'axios';
 
-function Map() {
-    return(
-        <GoogleMap defaultZoom={10} defaultCenter={{lat: 40.2046, lng:-77.4977}} />
-    );
+const containerStyle = {
+    width: "400px",
+    height: "400px",
+  };
+
+export default function Map() {
+    const [currentLoc, setCurrentLoc] = React.useState({
+        lat: 42.331429,
+        lng: -83.045753
+    })
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          setCurrentLoc({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          });
+        });
+      }
+      
+  return (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={{ lat: currentLoc.lat, lng: currentLoc.lng }}
+        zoom={10}
+      >
+        {/* Child components, such as markers, info windows, etc. */}
+        <></>
+      </GoogleMap>
+  );
 }
 
-export default Map;
+
