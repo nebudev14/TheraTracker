f8ed7f0513f63a1f01de1a8553075e4dd8dc58f5
warren yun <xxwarrenhdxx@gmail.com>
2021-05-22 23:03:57 -0400

    finished all connections, converted map function into class, fixed map error

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,47 +1,65 @@
 import React from 'react';
 import { GoogleMap, LoadScript } from 'react-google-maps';
 import axios from 'axios';
+import { render } from 'react-dom';
 
 const containerStyle = {
     width: "400px",
     height: "400px",
   };
 
-export default function Map() {
-    const [currentLoc, setCurrentLoc] = React.useState({
-      lat: 42.331429,
-      lng: -83.045753
-    })
+export default class Map extends React.Component {
 
-    const [therapists, addTherapists] = React.useState({
-      therapistList: []
-    })
+    constructor(props) {
+      super(props);
 
-    if (navigator.geolocation) {
+      this.state = {
+        lat: 42.331429,
+        lng: -83.045753,
+        therapistList: [],
+        psychiatristList: []
+      }
+
+    }
+
+    componentDidMount() {
+      if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((position) => {
-          setCurrentLoc({
+          this.setState({
             lat: position.coords.latitude,
             lng: position.coords.longitude,
           });
         });
       }
-    axios.get('http://127.0.0.1:5000/therapist/' + currentLoc.lat.toString() + '/' + currentLoc.lng.toString())
+
+      axios.get('http://127.0.0.1:5000/therapist/' + this.state.lat.toString() + '/' + this.state.lng.toString())
       .then(response => {
-        addTherapists({
+        this.setState({
           therapistList: response
         });
       });
 
-  return (
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={{ lat: currentLoc.lat, lng: currentLoc.lng }}
-        zoom={10}
-      >
-        {/* Child components, such as markers, info windows, etc. */}
-        <></>
-      </GoogleMap>
-  );
+      axios.get('http://127.0.0.1:5000/psychiatrist/' + this.state.lat.toString() + '/' + this.state.lng.toString())
+      .then(response => {
+        this.setState({
+          psychiatristList: response
+        });
+      });
+
+    }
+
+  render() {   
+    return (
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={{ lat: this.state.lat, lng: this.state.lng }}
+          zoom={10}
+        >
+          {/* Child components, such as markers, info windows, etc. */}
+          <></>
+        </GoogleMap>
+    );
+  }
 }
 
 
